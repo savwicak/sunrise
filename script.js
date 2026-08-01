d01b54509bcff9eb5829f1c4958597670cd1b3da
sapu <savwicak@gmail.com>
2026-08-01 22:58:54 +0700

    add little update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 function ClickHello() {
-    alert("Hello!")
+    alert("HIIII! if you want to be friend on steam press ok")
+    window.open("https://steamcommunity.com/id/overloadtask", "_blank");
 }
